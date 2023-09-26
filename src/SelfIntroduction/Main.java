51b2af2a20b574acc63a488277b42dc50661076b
kouki <eva.kk1127@gmail.com>
2023-09-27 03:41:47 +0900

    カリキュラム３０提出

--- src/SelfIntroduction/Main.java
+++ src/SelfIntroduction/Main.java
@@ -1,10 +1,15 @@
 package SelfIntroduction;
+
 class Main {
-	  public static void main(String[] args) {
-	    Person person1 = new Person("鈴木太郎", 20, 1.7);
-	   
-	    System.out.println(person1.name);
-	    System.out.println(person1.age);
-	    System.out.println(person1.height);
-	  }
+	public static void main(String[] args) {
+
+		// プロフィールの入力
+		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
+
+		// インスタンスメソッドの出力
+		Person.print(person1);
+
+		// 合計人数を出力
+		System.out.println("合計" + Person.getTotalPeople() + "人です");
 	}
+}
--- src/SelfIntroduction/Person.java
+++ src/SelfIntroduction/Person.java
@@ -1,15 +1,51 @@
 package SelfIntroduction;
-class Person{
-	
-	// インスタンスフィールドを定義
-	String name;
-	int age;
-	double height;
 
-	  // コンストラクタを定義しインスタンスフィールドに値をセット
-	  Person(String name, int age, double height) {
-	    this.name = name;
-	    this.age = age;
-	    this.height = height;
-	  }
+class Person {
+
+	// インスタンスフィールドを定義
+	static String name;
+	static int age;
+	static double height;
+	static double weight;
+
+	// 合計人数を格納
+	static int totalPeople = 0;
+
+	// コンストラクタの定義
+	public Person(String name, int age, double height, double weight) {
+		Person.name = name;
+		Person.age = age;
+		Person.height = height;
+		Person.weight = weight;
+
+		// 人数のカウント
+		totalPeople++;
+	}
+
+	// BMIを割り出す計算
+	public static double bmi() {
+		double bmi = weight / (height * height);
+		return Math.floor(bmi * 1.0) / 1.0;
+	}
+
+	// インスタンスメソッドの定義
+	public static void print(Person person) {
+		System.out.println("名前は" + getName() + "です");
+		System.out.println("BMIは" + String.format("%.1f", bmi()) + "です");
+		System.out.println("年は" + getAge() + "です");
+	}
+
+	// getter
+	public static String getName() {
+		return name;
+	}
+	public static int getAge() {
+		return age;
+	}
+	public double getHeight() {
+		return height;
+	}
+	public static int getTotalPeople() {
+		return totalPeople;
+	}
 }
